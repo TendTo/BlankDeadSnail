3bece003dd4a51c3e993d758c2802e06a050a2e7
DomDomo <g.domis02@gmail.com>
2023-11-04 16:05:08 +0000

    Add right and left shelf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,34 +5,65 @@ import { useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@rea
 import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
 import getUuid from 'uuid-by-string'
+import React from 'react'
 
 const GOLDENRATIO = 1.61803398875
 
 export const App = ({ images }) => (
   <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
-    <color attach="background" args={['#191920']} />
-    <fog attach="fog" args={['#191920', 0, 15]} />
+    <color attach="background" args={['#FFFFFF']} />
     <group position={[0, -0.5, 0]}>
-      <Frames images={images} />
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[50, 50]} />
-        <MeshReflectorMaterial
-          blur={[300, 100]}
-          resolution={2048}
-          mixBlur={1}
-          mixStrength={80}
-          roughness={1}
-          depthScale={1.2}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          color="#050505"
-          metalness={0.5}
-        />
-      </mesh>
+      {/* <Frames images={images} /> */}
+      <Shelf />
+      <Floor />
     </group>
     <Environment preset="city" />
   </Canvas>
 )
+
+function Shelf({ q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
+  p.set(0, 0, 5.5)
+  q.identity()
+
+  useFrame((state, dt) => {
+    easing.damp3(state.camera.position, p, 0.4, dt)
+    easing.dampQ(state.camera.quaternion, q, 0.4, dt)
+  })
+
+  return (
+    <group>
+      <mesh name="rightShelf" scale={[20, GOLDENRATIO, 0.05]} position={[1, GOLDENRATIO / 2, 0]} rotation={[0, -Math.PI / 2, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
+      </mesh>
+      <mesh name="leftShelf" scale={[20, GOLDENRATIO, 0.05]} position={[-1, GOLDENRATIO / 2, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
+      </mesh>
+    </group>
+  )
+}
+
+function Floor() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[50, 50]} />
+      <MeshReflectorMaterial
+        blur={[300, 100]}
+        resolution={2048}
+        mixBlur={1}
+        mixStrength={80}
+        roughness={1}
+        depthScale={1.2}
+        minDepthThreshold={0.4}
+        maxDepthThreshold={1.4}
+        color="#050505"
+        metalness={0.5}
+        mirror={0}
+      />
+    </mesh>
+  )
+}
 
 function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
   const ref = useRef()
@@ -69,11 +100,14 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
   const [rnd] = useState(() => Math.random())
   const name = getUuid(url)
   const isActive = params?.id === name
+
+  const targetScaleX = !isActive && hovered ? 0.85 * 0.85 : 0.85
+  const targetScaleY = !isActive && hovered ? 0.905 * 0.9 : 0.9
+
   useCursor(hovered)
   useFrame((state, dt) => {
-    image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-    easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
-    easing.dampC(frame.current.material.color, hovered ? 'orange' : 'white', 0.1, dt)
+    image.current.material.zoom = 2
+    easing.damp3(image.current.scale, [targetScaleX, targetScaleY, 1], 0.1, dt)
   })
   return (
     <group {...props}>
@@ -81,19 +115,12 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
         name={name}
         onPointerOver={(e) => (e.stopPropagation(), hover(true))}
         onPointerOut={() => hover(false)}
-        scale={[1, GOLDENRATIO, 0.05]}
+        scale={[10, GOLDENRATIO, 0.05]}
         position={[0, GOLDENRATIO / 2, 0]}>
         <boxGeometry />
         <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-        <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
-          <boxGeometry />
-          <meshBasicMaterial toneMapped={false} fog={false} />
-        </mesh>
         <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
       </mesh>
-      <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
-        {name.split('-').join(' ')}
-      </Text>
     </group>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { App } from './App'
 
-const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
+const pexel = (id) => `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQxP82MWQIP3FK6vB2SbN9u9BeUydFDPQx5tVgjqaX&s`
 const images = [
-  // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
-  // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
   { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
   { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
 ]
 
